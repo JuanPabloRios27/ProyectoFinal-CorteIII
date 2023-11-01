7438a5d06ae3699a9a0eff8f8981982aa4229288
World Wide Network <jprios@unbosque.edu.co>
2023-11-01 14:09:47 -0500

    investigando

---
+++ CorteIII-ProyectoFinal/src/main/java/co/edu/unbosque/model/Operaciones.java
@@ -0,0 +1,8 @@
+package co.edu.unbosque.model;
+
+public interface Operaciones {
+	public void crear();
+	public void eliminar();
+	public void actualizar();
+	public String mostrar();
+}
---
+++ CorteIII-ProyectoFinal/src/main/java/co/edu/unbosque/model/usuarioDAO.java
@@ -0,0 +1,38 @@
+package co.edu.unbosque.model;
+
+import java.util.ArrayList;
+
+public class usuarioDAO implements Operaciones{
+	
+	public ArrayList<usuario> ListaUsuario;
+	
+	public usuarioDAO() {
+		
+	}
+	@Override
+	public void crear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actualizar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String mostrar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
